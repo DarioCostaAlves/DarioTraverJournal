58a09c32e5784350c714ed149f32c017ca50692e
DarioCostaAlves <dario.c.alves22@gmail.com>
2022-05-16 19:21:08 +0100

    Card design. props

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import Travel from './components/Travel'
 import data from './data'
 
 export default function App() {
-        
+    const travels = data.map(item => {
+        return(
+            <Travel 
+                key={item.id} 
+                item={item} 
+            />
+        )
+    })
     return (
         <div>
-            <Navbar />            
-            <Travel />
+            <Navbar />      
+            <section className="card-list">
+                {travels}
+            </section>                  
         </div>
     )
 }
--- src/components/Travel.js
+++ src/components/Travel.js
@@ -1,19 +1,19 @@
 import React from 'react'
 
-export default function Travel() {
+export default function Travel(props) {
     return (        
-        <div className="travelCard">
+        <div className="card">
             <div className="card-left">
-                <img className="card-pic" src="https://twomonkeystravelgroup.com/wp-content/uploads/2020/01/15-Best-Things-To-Do-in-Dortmund-Germany.jpg"/>
+                <img className="card-pic" src={props.item.photo}/>
             </div>
             <div className="card-right">
                 <div>
-                    <i>Portugal</i>
-                    <a href="#" className="">View on maps</a>
+                    <i className="card-country">{props.item.country}</i>
+                    <a class="card-maps" href={props.item.maps} target="_blank">View on maps</a>
                 </div>
-                <h1>Netherlands</h1>
-                <small>15 March, 2022 - 20 March, 2022</small>
-                <p>What is happening right here</p>     
+                <h1>{props.item.attraction}</h1>
+                <small>{props.item.beginDate} - {props.item.finishDate}</small>
+                <p>{props.item.description}</p>     
             </div>       
         </div>
     )
